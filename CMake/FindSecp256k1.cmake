3ba7029d718880c75342c64157dcd9f268eb4932
neodiX <neodiX@ton.org>
2023-01-27 09:08:19 +0100

    add support of secp256k1 library to the builds (linux, win)

---
+++ CMake/FindSecp256k1.cmake
@@ -0,0 +1,25 @@
+# - Try to find SECP256K1
+# Once done this will define
+#
+#  SECP256K1_FOUND - system has SECP256K1
+#  SECP256K1_INCLUDE_DIRS - the SECP256K1 include directory
+#  SECP256K1_LIBRARY - Link these to use SECP256K1
+
+find_path(
+    SECP256K1_INCLUDE_DIR
+    NAMES secp256k1_recovery.h
+    DOC "secp256k1_recovery.h include dir"
+)
+
+find_library(
+    SECP256K1_LIBRARY
+    NAMES secp256k1
+    DOC "secp256k1 library"
+)
+
+set(SECP256K1_INCLUDE_DIRS ${SECP256K1_INCLUDE_DIR})
+set(SECP256K1_LIBRARIES ${SECP256K1_LIBRARY})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(SECP256K1 DEFAULT_MSG SECP256K1_INCLUDE_DIR SECP256K1_LIBRARY)
+mark_as_advanced(SECP256K1_INCLUDE_DIR SECP256K1_LIBRARY)
