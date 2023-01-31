fef396d7ea31347b562c23ed3dd1add350d99671
neodiX <neodiX@ton.org>
2023-02-01 00:45:52 +0100

    add ninja build windows

--- CMake/FindSodiumAndroid.cmake
+++ CMake/FindSodiumAndroid.cmake
@@ -3,7 +3,7 @@
 #
 #  SODIUM_FOUND - system has SODIUM
 #  SODIUM_INCLUDE_DIRS - the SODIUM include directory
-#  SODIUM_LIBRARY - Link these to use SODIUM
+#  SODIUM_LIBRARY_RELEASE - Link these to use SODIUM
 
 
 find_path(
@@ -12,17 +12,17 @@ find_path(
 )
 
 find_library(
-    SODIUM_LIBRARY
+    SODIUM_LIBRARY_RELEASE
     NAMES libsodium.a libsodium.lib
 )
 
-if (SODIUM_LIBRARY)
-  message(STATUS "Found Sodium: ${SODIUM_LIBRARY}")
+if (SODIUM_LIBRARY_RELEASE)
+  message(STATUS "Found SodiumAndroid: ${SODIUM_LIBRARY_RELEASE}")
 endif()
 
 #set(SODIUM_INCLUDE_DIRS ${SODIUM_INCLUDE_DIR})
-#set(SODIUM_LIBRARIES ${SODIUM_LIBRARY})
+#set(SODIUM_LIBRARIES ${SODIUM_LIBRARY_RELEASE})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SodiumAndroid DEFAULT_MSG SODIUM_INCLUDE_DIR SODIUM_LIBRARY)
-mark_as_advanced(SODIUM_INCLUDE_DIR SODIUM_LIBRARY)
+find_package_handle_standard_args(SodiumAndroid DEFAULT_MSG SODIUM_INCLUDE_DIR SODIUM_LIBRARY_RELEASE)
+mark_as_advanced(SODIUM_INCLUDE_DIR SODIUM_LIBRARY_RELEASE)
