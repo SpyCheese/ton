24d4f423b87406a3ffba679243e7c27c52731518
neodiX <neodiX@ton.org>
2023-02-02 13:26:35 +0100

    refactor llvm-strip location

--- example/android/build-all.sh
+++ example/android/build-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo Building tonlib for x86...
 echo
-#ARCH="x86" ./build.sh || exit 1
+ARCH="x86" ./build.sh || exit 1
 
 echo Building tonlib for x86_64...
 echo
@@ -9,7 +9,7 @@ ARCH="x86_64" ./build.sh || exit 1
 
 echo Building tonlib for arm...
 echo
-#ARCH="arm" ./build.sh || exit 1
+ARCH="arm" ./build.sh || exit 1
 
 echo Building tonlib for arm64...
 echo
