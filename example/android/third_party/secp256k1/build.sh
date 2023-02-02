a741eeb26ba03fc4d0d2633ab4fe307d09a8566a
neodiX <neodiX@ton.org>
2023-02-02 13:02:11 +0100

    update precompiled with NDK libsecp256k1.a

--- example/android/third_party/secp256k1/build.sh
+++ example/android/third_party/secp256k1/build.sh
@@ -6,9 +6,9 @@ cd secp256k1
 ./autogen.sh
 #./configure --enable-module-recovery
 #./configure --enable-module-recovery --with-asm=arm --enable-experimental
-./configure --enable-experimental --with-asm=arm --host=arm-linux-androideabi CC=armv7a-linux-androideabi21-clang CFLAGS="-mthumb -march=armv7-a" CCASFLAGS="-Wa,-mthumb -Wa,-march=armv7-a"
-#./configure --enable-experimental --with-asm=arm --host=arm-linux-androideabi CC=armv8a-linux-androideabi21-clang CFLAGS="-mthumb -march=armv8-a" CCASFLAGS="-Wa,-mthumb -Wa,-march=armv8-a"
-#./configure --host=x86_64-linux-androideabi CC=x86_64-linux-android21-clang
-#./configure --host=i686-linux-androideabi CC=i686-linux-android21-clang
+./configure --enable-module-recovery --enable-experimental --with-asm=arm --host=arm-linux-androideabi CC=armv7a-linux-androideabi21-clang CFLAGS="-mthumb -march=armv7-a" CCASFLAGS="-Wa,-mthumb -Wa,-march=armv7-a"
+#./configure --enable-module-recovery --enable-experimental --with-asm=arm --host=arm-linux-androideabi CC=armv8a-linux-androideabi21-clang CFLAGS="-mthumb -march=armv8-a" CCASFLAGS="-Wa,-mthumb -Wa,-march=armv8-a"
+#./configure --enable-module-recovery --host=x86_64-linux-androideabi CC=x86_64-linux-android21-clang
+#./configure --enable-module-recovery --host=i686-linux-androideabi CC=i686-linux-android21-clang
 make
 #make install
