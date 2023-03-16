13e7256e584e1524551e69a50b683277d1b91d11
neodiX <neodix@ton.org>
2023-03-16 19:19:49 +0100

    add secp256k1 to nix

--- flake.nix
+++ flake.nix
@@ -32,12 +32,14 @@
               zlib
               libmicrohttpd
               libsodium
+              secp256k1
             ] else
               [
                 (openssl_1_1.override { static = true; }).dev
                 (zlib.override { shared = false; }).dev
                 pkgsStatic.libmicrohttpd.dev
                 pkgsStatic.libsodium.dev
+                secp256k1
               ] ++ optional staticGlibc glibc.static;
 
           cmakeFlags = [ "-DTON_USE_ABSEIL=OFF" "-DNIX=ON" ] ++ optionals staticMusl [
